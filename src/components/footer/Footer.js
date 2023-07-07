2d3dd538709a28b8f0f5c525bf21f9d6e54ddc60
n0is9 <zaharchukartem9@gmail.com>
2023-07-07 12:17:02 +0300

    updated code

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,12 +1,29 @@
 import React from "react";
-
+import { NavLink } from "react-router-dom";
 import './style.css'
-import instagram from "../../img/icons/instagram.svg";
-import twitter from "../../img/icons/twitter.svg";
+// import instagram from "../../img/icons/instagram.svg";
+// import twitter from "../../img/icons/twitter.svg";
 import gitHub from "../../img/icons/gitHub.svg";
 import linkedIn from "../../img/icons/linkedIn.svg";
 import telegram from "../../img/icons/telegram.svg";
-import {NavLink} from "react-router-dom";
+
+const socialMediaLinks = [
+    {
+        platform: "GitHub",
+        url: "https://github.com/n0is9",
+        icon: gitHub,
+    },
+    {
+        platform: "LinkedIn",
+        url: "https://www.linkedin.com/in/artem-zakharchuk-39aa66211/",
+        icon: linkedIn,
+    },
+    {
+        platform: "Telegram",
+        url: "https://t.me/n0is9",
+        icon: telegram,
+    },
+];
 
 function Footer() {
     return (
@@ -14,35 +31,13 @@ function Footer() {
             <div className="container">
                 <div className="footer__wrapper">
                     <ul className="social">
-
-                        <li className="social__item">
-                            <NavLink to='https://github.com/n0is9'target='_blank' rel='noreferrer'>
-                                <img src={gitHub} alt="Link"/>
-                            </NavLink>
-                        </li>
-                        <li className="social__item">
-                            <NavLink to='https://www.linkedin.com/in/artem-zakharchuk-39aa66211/'target='_blank' rel='noreferrer'>
-                                <img src={linkedIn} alt="Link"/>
-                            </NavLink>
-                        </li>
-
-                        <li className="social__item">
-                            <NavLink to='https://t.me/n0is9' target='_blank' rel='noreferrer'>
-                                <img src={telegram} alt="Link"/>
-                            </NavLink>
-                        </li>
-                        <li className="social__item">
-                            <NavLink to='https://www.instagram.com/arteemzaharchuk/?hl=uk' target='_blank' rel='noreferrer'>
-                                <img src={instagram} alt="Link"/>
-                            </NavLink>
-                        </li>
-                        <li className="social__item">
-                            <NavLink to='https://twitter.com/noiiiiisy'target='_blank' rel='noreferrer'>
-                                <img src={twitter} alt="Link"/>
-                            </NavLink>
-                        </li>
-
-
+                        {socialMediaLinks.map((link, index) => (
+                            <li className="social__item" key={index}>
+                                <NavLink to={link.url} target="_blank" rel="noreferrer">
+                                    <img src={link.icon} alt={link.platform} />
+                                </NavLink>
+                            </li>
+                        ))}
                     </ul>
                     <div className="copyright">
                         <p>© 2023 developed by artem zakharchuk</p>
@@ -50,7 +45,8 @@ function Footer() {
                 </div>
             </div>
         </footer>
-    )
+    );
 }
+
 
 export default Footer
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -20,8 +20,6 @@ function Projects() {
                             />
                         )
                     })}
-
-
                 </ul>
             </div>
         </main>
